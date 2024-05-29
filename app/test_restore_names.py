277f1de75d6246f045bde275347f12c74f0572ea
Vladyslav Shabas <vladyslav.shabas@visahq.com>
2024-05-29 17:25:52 +0300

    solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -25,4 +25,4 @@ def prepare_test_users() -> list[dict]:
 
 def test_restore_names(prepare_test_users: list[dict]) -> None:
     restore_names(prepare_test_users)
-    assert prepare_test_users == [{'first_name': 'Jack', 'last_name': 'Holy', 'full_name': 'Jack Holy'}, {'last_name': 'Adams', 'full_name': 'Mike Adams', 'first_name': 'Mike'}, {'first_name': 'Jack', 'last_name': 'Holy', 'full_name': 'Jack Holy'}]
+    assert prepare_test_users == [{"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}, {"last_name": "Adams", "full_name": "Mike Adams", "first_name": "Mike"}, {"first_name": "Jack", "last_name": "Holy", "full_name": "Jack Holy"}]
